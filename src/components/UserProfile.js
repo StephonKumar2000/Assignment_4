f3dfd82f8d77f5b734f5bbb685849f78ccb0ca96
Stephon Kumar <stephon.kumar13@myhunter.cuny.edu>
2021-11-21 12:31:09 -0500

    user profile js added

---
+++ src/components/UserProfile.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
+
+class UserProfile extends Component {
+  render() {
+    return (
+        <div>
+          <h4>User Profile</h4>
+          <div>Username: {this.props.userName}</div>
+          <div>Member Since: {this.props.memberSince}</div>
+        </div>
+    );
+  }
+}
+
+export default UserProfile;
